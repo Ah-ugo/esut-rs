a056e0cbd54db5c05cab64287a8c383156652539
mac <ahuekweprinceugo@gmail.com>
2026-05-24 12:48:12 +0100

    cors fix

--- app/main.py
+++ app/main.py
@@ -33,7 +33,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://esut-transcript.vercel.app/"],
+    allow_origins=["http://localhost:3000", "https://esut-transcript.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
